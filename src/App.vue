7b8bde1432ee397fd9e21f1e7c7c94e080e6aaae
Abdullah <abdullah@detikideal.com.my>
2023-08-10 15:38:30 +0800

    add

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+
+<template>
+  <router-view />
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: "App",
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir,Helvetica,Arial,sans-serif;
+}
+</style>
+
+<!-- <script setup>
+import MainLayout from './layouts/MainLayout.vue';
+import HomeSec from './components/HomeSec.vue';
+import ProductSec from './components/ProductSec.vue';
+import AboutSec from './components/AboutSec.vue';
+import ContactSec from './components/ContactSec.vue';
+import Footer from './components/FooterSec.vue';
+</script>
+
+<template>
+  <div>
+    <MainLayout/>
+    <HomeSec/>
+    <ProductSec/>
+    <AboutSec/>
+    <ContactSec/>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
+
+export default defineComponent({
+  name: "App",
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir,Helvetica,Arial,sans-serif;
+}
+</style> -->
