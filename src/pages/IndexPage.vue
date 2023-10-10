41e9fbb7e65f7c841d4cc4452d7244ae3acc1f49
AbdulDean <132578851+AbdulDean@users.noreply.github.com>
2023-10-10 12:03:21 +0800

    Update IndexPage.vue

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -13,7 +13,7 @@
                     <div class="column" style="">
                       <p class="text-weight-bold text-white text-h2" style="font-size: 50px;">
                         Git Action
-                        <br>Is Awesome and Make Your Life Easier
+                        <br>Is Awesome and Make Your Life Easier and Faster
                       </p>
                       <p class="text-white q-pt-sm" style="font-size: 17px; font-weight: 400;">
                         We offer smart, creative and modern IT solutions to deliver your vision.
